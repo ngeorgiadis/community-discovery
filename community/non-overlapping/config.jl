4da0930d5502c42d747b76ff53592808c302f7aa
Nikolaos Georgiadis <ng@ipquants.com>
2022-10-01 09:51:37 +0300

    config

--- community/non-overlapping/config.jl
+++ community/non-overlapping/config.jl
@@ -1,4 +1,8 @@
 
-all = 500000
-hop = 2
-dom_file = "/mnt/n/sources/01_datalab/2022/data/auth_dom_scores.txt"
+all = 10000
+hop = 1
+#dom_file = "/mnt/n/sources/01_datalab/2022/data/appx-dom-4dim-g1000.txt"
+
+# this is the exact dom score
+# dom_file = "/mnt/n/sources/01_datalab/2022/data/auth_dom_scores.txt"
+dom_file = "/mnt/n/sources/01_datalab/2022/data/appx-500-new-z.txt"
